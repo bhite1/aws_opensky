64dd630626e41cf384bd3e4f52fc1cfc9ed8f01e
pgandyai <pgandy@applied-insight.com>
2023-08-24 09:37:22 -0400

    bucket names are lower case

--- cloudformation/PrimayDeploymentScript.sh
+++ cloudformation/PrimayDeploymentScript.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Set AWS CLI default region (change as needed)
-region="us-east-1"
-bucketName="HackyBucketsAreNeat"
+region="us-west-2"
+bucketName="hackybucketsareneat"
 stackName="ParentStack"
-deployTemplate="./deploy.json"
+deployTemplate="./Deploy.json"
 
 # Create S3 bucket
 aws s3api create-bucket --bucket $bucketName --region $region
+
+# Wait for the S3 bucket to be confirmed as existing
+echo "Waiting for bucket to be created..."
+while [[ $(aws s3api head-bucket --bucket $bucketName 2>&1) ]]; do
+  echo -n '.'
+  sleep 2
+done
+echo -e "\nBucket created."
 
 # Sync CloudFormation child templates to the S3 bucket
 aws s3 cp ./dynamodb.json s3://$bucketName/
